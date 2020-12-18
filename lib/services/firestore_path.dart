588aec74edb2c5f757d5c4c1db51089f5d7b4768
Kehinde Akeredolu <9991881+kayzkenny@users.noreply.github.com>
2020-12-18 09:36:21 +0100

    Create firestore path file

---
+++ lib/services/firestore_path.dart
@@ -0,0 +1,12 @@
+class FirestorePath {
+  static String users() => 'users';
+  static String recipes() => 'recipes';
+  static String cuisines() => 'cuisines';
+  static String cookbooks() => 'cookbooks';
+  static String categories() => 'categories';
+  static String userData(String uid) => 'users/$uid';
+  static String recipe(String recipeId) => 'recipes/$recipeId';
+  static String userCookbooks(String cookbookID) => 'cookbooks/$cookbookID';
+  static String recipeUserRating(String recipeId, String uid) =>
+      'recipes/$recipeId/ratings/$uid';
+}
