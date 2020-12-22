041ed78efac464f5bb54d9fbee02b3e5246a48c0
Kehinde Akeredolu <9991881+kayzkenny@users.noreply.github.com>
2020-12-22 10:32:22 +0100

    create user favourites path variable

--- lib/services/firestore_path.dart
+++ lib/services/firestore_path.dart
@@ -6,6 +6,7 @@ class FirestorePath {
   static String categories() => 'categories';
   static String userData(String uid) => 'users/$uid';
   static String recipe(String recipeId) => 'recipes/$recipeId';
+  static String userFavourites(String uid) => 'users/$uid/favourites';
   static String userCookbooks(String cookbookID) => 'cookbooks/$cookbookID';
   static String recipeUserRating(String recipeId, String uid) =>
       'recipes/$recipeId/ratings/$uid';
